5ca9c9158be2b0bdef36b64fbb2b540d67f6d6ff
Russell Bewley <rbewley4@gmail.com>
2014-07-27 22:43:44 -0700

    #8 update about popup with github info

--- src/Creator/version4/popup-contents/about.php
+++ src/Creator/version4/popup-contents/about.php
@@ -1,4 +1,6 @@
 <table id="table_reset" align="center">
+    <a href="https://github.com/rbewley4/ep-character-creator" target="_blank"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>
+
     <tr align="center">
         <td>
             <h1><b><u> About</u></b></h1>
@@ -7,34 +9,32 @@
 
     <tr align="center">
         <td>
+            Eclipse Phase Character Creator<br>
+            <small> Version 1.0 (03.2014)</small>
+            <br><br>
+            A character creator for the <a href="http://eclipsephase.com" target="_blank">Eclipse Phase</a> role-playing game.
+            <br><br>
+            Please submit all suggestions and bug reports to the <br>
+            GitHub <a href="https://github.com/rbewley4/ep-character-creator/issues" target="_blank">Issues</a> page.
+            <br><br>
+        </td>
+    </tr>
+
+    <tr align="center">
+        <td>
             GUI-code-sql : <b>Cédric Reinhardt</b><br>
             Code-game mechanics : <b>Jigé Pont</b><br>
             Graphic concept : <b>Olivier Murith</b><br>
-            <br><br>
-            For the RPG : <a href="http://eclipsephase.com">EClipse Phase</a>
-            <br><br>
-        </td>
-    </tr>
-
-    <tr align="center">
-        <td>
-            You can download this work for installing it in your own server. If you make some changes or improvements please let us know.<br>
-            <a href="http://eclipsephase.oook.ch/EPCC_sources.zip">EPCC V 1.0 (03.2014)</a>
-            <br><br>
-            <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a><br /><small>  This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>.</small>
+            <br>
+            <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US" target="_blank"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a><br /><small>  This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US" target="_blank">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>.</small>
             <br><br>
         </td>
     </tr>
 
     <tr align="center">
         <td>
-            For all suggestions or bug report please write to <a href="mailto:game@oook.ch"> game@oook.ch</a>
-        </td>
-    </tr>
-
-    <tr align="center">
-        <td>
-            <small> Version 1.0 (03.2014)</small>
+            You can download this work and install it in your own server. If you make some changes or improvements, please let us know.<br>
+            <a href="https://github.com/rbewley4/ep-character-creator/releases" target="_blank">EPCC Releases</a>
             <br><br>
         </td>
     </tr>
