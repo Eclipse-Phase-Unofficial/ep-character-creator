53ad3f2e6b856392f409304bba21dd07878026b1
Arthur Moore <Arthur.Moore.git@cd-net.net>
2016-12-12 09:31:32 -0500

    Add more background images

--- src/Creator/version4/index.php
+++ src/Creator/version4/index.php
@@ -261,7 +261,17 @@
                     slides: [
                         { src: 'img/bg/bg1.jpg'},
                         { src: 'img/bg/bg2.jpg'},
-                        { src: 'img/bg/bg3.jpg'}
+                        { src: 'img/bg/bg3.jpg'},
+                        //These are all free (at least non-commercial) use images or images in the public domain
+                        { src: 'https://upload.wikimedia.org/wikipedia/commons/3/34/SFO_at_night.jpg'},     //Andrew Choy from Santa Clara, California (Creative Commons Attribution-Share Alike 2.0 Generic)
+                        { src: 'https://upload.wikimedia.org/wikipedia/commons/6/62/Starsinthesky.jpg'},    //Credit ESA (This is me giving credit, per the license)
+                        { src: 'https://upload.wikimedia.org/wikipedia/commons/0/00/Crab_Nebula.jpg'},                  //Credit NASA
+                        { src: 'https://upload.wikimedia.org/wikipedia/commons/7/7f/Ngc1999.jpg'},                      //Credit NASA
+                        { src: 'https://upload.wikimedia.org/wikipedia/commons/4/4e/Pleiades_large.jpg'},               //Credit NASA
+                        { src: 'https://upload.wikimedia.org/wikipedia/commons/c/c9/Sirius_A_and_B_artwork.jpg'},       //Credit NASA
+                        { src: 'https://upload.wikimedia.org/wikipedia/commons/5/57/Witness_the_Birth_of_a_Star.jpg'},  //Credit NASA
+                        { src: 'https://upload.wikimedia.org/wikipedia/commons/4/44/Ngc6397_hst_blue_straggler.jpg'},   //Credit NASA
+                        { src: 'https://upload.wikimedia.org/wikipedia/commons/b/b4/The_Sun_by_the_Atmospheric_Imaging_Assembly_of_NASA%27s_Solar_Dynamics_Observatory_-_20100819.jpg'}  //Credit NASA
                     ]
                 });
             });
