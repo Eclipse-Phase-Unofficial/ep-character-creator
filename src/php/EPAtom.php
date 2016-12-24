Eclipse-Phase-Unofficial/ep-character-creator

commit 20d2b70b7979fda0ca5480ce21233a3dec7ef11f

Atom helper function cleanup
Moved Atom specific functions from EPCharacterCreator to EPAtom.
Use OOP for Atom funcitons where appropriate. (Actually makes lines smaller and more readable)
Do all Atom in array checking by Uid, not name.
Replaced redundant code with calls to appropriate Atom helper function.
Removed dead code.

src/php/EPAtom.php
@@ -118,10 +118,78 @@ class EPAtom {
         return $this->atomUid;
     }
 
+    //Strip any character that could cause an issue in an id tag
     private function sanitize($input){
         $replace_char = '/[^A-Z,^a-z,^0-9]/';
         return preg_replace($replace_char, '_', $input);
     }
+
+    // Check if this Atom is in the array
+    public function isInArray($array){
+        if (!empty($array)){
+            foreach ($array as $item){
+                if (strcmp($item->getUid(),$this->atomUid) == 0){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Add this Atom to an array, returns true when done, or if this atom was already in the array
+    public function addToArray(&$array){
+        if (is_array($array)){
+            if (!$this->isInArray($array)){
+                array_push($array, $this);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    // Remove this Atom from an array, returns false on failure
+    public function removeFromArray(&$array){
+        if (!$this->isInArray($array)){
+            return false;
+        }else{
+            $index = 0;
+            foreach ($array as $value) {
+                if (strcmp($value->getUid(),$this->getUid()) == 0){
+                    break;
+                }else{
+                    $index++;
+                }
+            }
+            array_splice($array, $index, 1);
+            return true;
+        }
+    }
+}
+
+//**********HELPER FUNCTIONS**********//
+
+// Find an Atom with a particular name (potentially dangerous, do not use for skills)
+function getAtomByName($array,$name){
+    if(!empty($array)){
+        foreach ($array as $a){
+            if (strcmp($a->name,$name) == 0){
+                return $a;
+            }
+        }
+    }
+    return null;
+}
+
+// Find an atom by unique id (safe)
+function getAtomByUid($array,$id){
+    if(!empty($array)){
+        foreach ($array as $a){
+            if (strcmp($a->getUid(),$id) == 0){
+                return $a;
+            }
+        }
+    }
+    return null;
 }
 
 ?>

src/Creator/version4/quaternary-choice/gearMorphBMD.php
@@ -1,5 +1,6 @@
 <?php
 require_once '../../../php/EPCharacterCreator.php'; //BMD stand for : Bonus Malus Description
+require_once '../../../php/EPAtom.php';
 include('../other/bonusMalusLayer.php');
 include('../other/armorDegatsLayer.php');
 include('../other/bookPageLayer.php');
@@ -9,7 +10,7 @@ session_start();
 
 // $hint = "--dubug:morph";
 $morphGears = $_SESSION['cc']->getGearForMorphName($_SESSION['currentMorph']);
-$currentGear = $_SESSION['cc']->getAtomByName($morphGears,$_SESSION['currentMorphGearName']);
+$currentGear = getAtomByName($morphGears,$_SESSION['currentMorphGearName']);
 if($currentGear == null){
     $currentGear =  $_SESSION['cc']->getGearByName($_SESSION['currentMorphGearName']);
     // $hint = "--debug:general";

src/Creator/version4/quaternary-choice/traitMorphBMD.php
@@ -1,12 +1,13 @@
 <?php
 require_once '../../../php/EPCharacterCreator.php'; //BMD stand for : Bonus Malus Description
+require_once '../../../php/EPAtom.php';
 include('../other/bonusMalusLayer.php');
 include('../other/bookPageLayer.php');
 
 session_start();
 
 $currentMorphTraits = $_SESSION['cc']->getCurrentMorphTraits($_SESSION['currentMorph']);
-$currentTrait = $_SESSION['cc']->getAtomByName($currentMorphTraits,$_SESSION['currentMorphTraitName']);
+$currentTrait = getAtomByName($currentMorphTraits,$_SESSION['currentMorphTraitName']);
 if($currentTrait == null){
     $currentTrait =  $_SESSION['cc']->getTraitByName($_SESSION['currentMorphTraitName']);
 }

src/Creator/version4/scripts/dispatcher.php
@@ -2,6 +2,7 @@
 
 require_once '../../../php/EPCharacterCreator.php';
 require_once '../../../php/EPListProvider.php';
+require_once '../../../php/EPAtom.php';
 require_once '../../../php/EPAptitude.php';
 require_once '../../../php/EPStat.php';
 require_once '../../../php/EPReputation.php';
@@ -176,7 +177,7 @@ if(isset($_POST['faction'])){
 if(isset($_POST['posTrait'])){
 	$trait = $_SESSION['cc']->getTraitByName($_POST['posTrait']);
 	if($trait != null){
-		if($_SESSION['cc']->isAtomInArrayByName($trait->name, $_SESSION['cc']->getCurrentTraits())){
+		if( $trait->isInArray( $_SESSION['cc']->getCurrentTraits() ) ){
 			if($_SESSION['cc']->removeTrait($trait)){
 				$_SESSION['currentTraitName'] = $trait->name;
 	        	$return['desc'] = $trait->description;
@@ -207,7 +208,7 @@ if(isset($_POST['posTraitHover'])){
 if(isset($_POST['psyS'])){
 	$psyS = $_SESSION['cc']->getPsySleightsByName($_POST['psyS']);
 	if($psyS != null){
-		if($_SESSION['cc']->isAtomInArrayByName($psyS->name, $_SESSION['cc']->getCurrentPsySleights())){
+		if( $psyS->isInArray( $_SESSION['cc']->getCurrentPsySleights() ) ){
 			if($_SESSION['cc']->removePsySleight($psyS)){
 	        	$return['desc'] = $psyS->description;
 	        	$_SESSION['currentPsiSName'] = $psyS->name;
@@ -235,7 +236,7 @@ if(isset($_POST['hoverPsyS'])){
 if(isset($_POST['negTrait'])){
 	$trait = $_SESSION['cc']->getTraitByName($_POST['negTrait']);
 	if($trait != null){
-		if($_SESSION['cc']->isAtomInArrayByName($trait->name, $_SESSION['cc']->getCurrentTraits())){
+		if($trait->isInArray($_SESSION['cc']->getCurrentTraits())){
                     if($_SESSION['cc']->removeTrait($trait)){
 			$_SESSION['currentTraitName'] = $trait->name;
 	        	$return['desc'] = $trait->description;
@@ -485,7 +486,7 @@ if(isset($_POST['remSpeSkill'])){
 
 //SET MORPH
 if (isset($_POST['addMorph'])) {
-	   $morph = $provider->getAtomByName($_SESSION['cc']->getMorphs(),$_POST['addMorph']);
+	   $morph = getAtomByName($_SESSION['cc']->getMorphs(),$_POST['addMorph']);
 	   if($_SESSION['cc']->addMorph($morph)){
                 $_SESSION['currentMorph'] =  $_POST['addMorph'];
                 $return['title'] = $morph->name;
@@ -498,7 +499,7 @@ if (isset($_POST['addMorph'])) {
 
 //HOVER MORPH
 if (isset($_POST['morphHover'])) {
-	   $morph = $provider->getAtomByName($_SESSION['cc']->getMorphs(),$_POST['morphHover']);
+	   $morph = getAtomByName($_SESSION['cc']->getMorphs(),$_POST['morphHover']);
        $return['title'] = $morph->name;
 	   $return['desc'] = $morph->description;
 }
@@ -507,7 +508,7 @@ if (isset($_POST['morphHover'])) {
 
 //REMOVE MORPH
 if (isset($_POST['remMorph'])) {
-	   $morph = $provider->getAtomByName($_SESSION['cc']->getMorphs(),$_POST['remMorph']);
+	   $morph = getAtomByName($_SESSION['cc']->getMorphs(),$_POST['remMorph']);
 	   if($_SESSION['cc']->removeMorph($morph)){
                 $return['desc'] = $morph->description;
 	   }
@@ -518,7 +519,7 @@ if (isset($_POST['remMorph'])) {
 
 //GET MORPH SETTINGS
 if (isset($_POST['morphSettings'])) {
-	   $morph = $provider->getAtomByName($_SESSION['cc']->character->morphs,$_POST['morphSettings']);
+	   $morph = getAtomByName($_SESSION['cc']->character->morphs,$_POST['morphSettings']);
 	   if($morph != null){
 	   		 $_SESSION['currentMorph'] =  $_POST['morphSettings'];
 	   		 $return['morphName'] = $morph->name;
@@ -542,7 +543,7 @@ if (isset($_POST['currentMorphUsed'])) {
 
 //SET MORPH SETTINGS
 if (isset($_POST['morphSettingsChange'])) {
-	   $morph = $provider->getAtomByName($_SESSION['cc']->getMorphs(),$_POST['morphSettingsChange']);
+	   $morph = getAtomByName($_SESSION['cc']->getMorphs(),$_POST['morphSettingsChange']);
 	   if($morph != null){
 	   		 $_SESSION['currentMorph'] =  $_POST['morphSettingsChange'];
 		     $morph->nickname = $_POST['morphNickname'];
@@ -927,24 +928,24 @@ if(isset($_POST['addTargetTo'])){
 		$bonusMalusArray = $currentBck->bonusMalus;
 	}
 	else if($_POST['parentType'] == "trait"){
-		$currentTrait = $_SESSION['cc']->getAtomByName($_SESSION['cc']->getCurrentTraits(),$_POST['parentName']);
+		$currentTrait = getAtomByName($_SESSION['cc']->getCurrentTraits(),$_POST['parentName']);
 		if(!isset($currentTrait)){
 			$currentTrait = $_SESSION['cc']->getTraitByName($_POST['parentName']);
 		}
 		$bonusMalusArray = $currentTrait->bonusMalus;
 	}
 	else if($_POST['parentType'] == "morph"){
-		$currentMorph = $_SESSION['cc']->getAtomByName($_SESSION['cc']->getCurrentMorphs(),$_POST['parentName']);
+		$currentMorph = getAtomByName($_SESSION['cc']->getCurrentMorphs(),$_POST['parentName']);
 		if(!isset($currentMorph)){
 			$currentMorph = $_SESSION['cc']->getMorphByName($_POST['parentName']);
 		}
 		$bonusMalusArray = $currentMorph->bonusMalus;
 	}
 	else if($_POST['parentType'] == "morphTrait"){
-		$currentMorph = $_SESSION['cc']->getAtomByName($_SESSION['cc']->getCurrentMorphs(),$_SESSION['currentMorph']);
+		$currentMorph = getAtomByName($_SESSION['cc']->getCurrentMorphs(),$_SESSION['currentMorph']);
         $traits = $_SESSION['cc']->getCurrentMorphTraits($currentMorph->name);
 		if (!empty($traits)){
-                    $currentMorphTrait = $_SESSION['cc']->getAtomByName($traits,$_POST['parentName']);
+                    $currentMorphTrait = getAtomByName($traits,$_POST['parentName']);
                     $bonusMalusArray = $currentMorphTrait->bonusMalus;          
                 }                
 	}
@@ -953,9 +954,9 @@ if(isset($_POST['addTargetTo'])){
 	}
 	
 	if($_POST['bMcase'] == EPBonusMalus::$MULTIPLE){
-		$candidatParent = $_SESSION['cc']->getBonusMalusByAtomeId($bonusMalusArray,$_POST['parentBmId']);
+		$candidatParent = getAtomByUid($bonusMalusArray,$_POST['parentBmId']);
 		if($candidatParent != null){
-			$candidat = $_SESSION['cc']->getBonusMalusByAtomeId($candidatParent->bonusMalusTypes,$_POST['bmId']);
+			$candidat = getAtomByUid($candidatParent->bonusMalusTypes,$_POST['bmId']);
 			if($candidat != null){
 				if($candidat->bonusMalusType == EPBonusMalus::$ON_SKILL){
 
@@ -981,7 +982,7 @@ if(isset($_POST['addTargetTo'])){
 	else{
             $candidat = null;
             if (!empty($bonusMalusArray)){
-                $candidat = $_SESSION['cc']->getBonusMalusByAtomeId($bonusMalusArray,$_POST['bmId']);   
+                $candidat = getAtomByUid($bonusMalusArray,$_POST['bmId']);
             }
 
             if($candidat != null){
@@ -1019,10 +1020,10 @@ if(isset($_POST['removeTargetFrom'])){
 		$bonusMalusArray = $currentMorph->bonusMalus;
 	}
 	else if($_POST['parentType'] == "morphTrait"){
-		$currentMorph = $_SESSION['cc']->getAtomByName($_SESSION['cc']->getCurrentMorphs(),$_SESSION['currentMorph']);
+		$currentMorph = getAtomByName($_SESSION['cc']->getCurrentMorphs(),$_SESSION['currentMorph']);
                 $traits = $_SESSION['cc']->getCurrentMorphTraits($currentMorph->name);
                 if (!empty($traits)){
-                    $currentMorphTrait = $_SESSION['cc']->getAtomByName($traits,$_POST['parentName']);
+                    $currentMorphTrait = getAtomByName($traits,$_POST['parentName']);
                     $bonusMalusArray = $currentMorphTrait->bonusMalus;                    
                 }
 	}
@@ -1030,9 +1031,9 @@ if(isset($_POST['removeTargetFrom'])){
 		treatCreatorErrors($return,new EPCreatorErrors("Unknown parent type",EPCreatorErrors::$SYSTEM_ERROR));
 	}
 	if($_POST['bMcase'] == EPBonusMalus::$MULTIPLE){
-		$candidatParent = $_SESSION['cc']->getBonusMalusByAtomeId($bonusMalusArray,$_POST['parentBmId']);
+		$candidatParent = getAtomByUid($bonusMalusArray,$_POST['parentBmId']);
 		if($candidatParent != null){
-			$candidat = $_SESSION['cc']->getBonusMalusByAtomeId($candidatParent->bonusMalusTypes,$_POST['bmId']);
+			$candidat = getAtomByUid($candidatParent->bonusMalusTypes,$_POST['bmId']);
 			if($candidat != null){
 				if(!empty($candidat->targetForChoice)){
 					$candidat->forTargetNamed = "";
@@ -1049,7 +1050,7 @@ if(isset($_POST['removeTargetFrom'])){
 		}
 	}
 	else{
-		$candidat = $_SESSION['cc']->getBonusMalusByAtomeId($bonusMalusArray,$_POST['bmId']);
+		$candidat = getAtomByUid($bonusMalusArray,$_POST['bmId']);
 		if($candidat != null){
 			$candidat->forTargetNamed = "";
 			$_SESSION['cc']->adjustAll();
@@ -1071,14 +1072,14 @@ if(isset($_POST['addOccurence'])){
 	}
 	
 	if($_POST['addOccurence'] == "SOFT"){
-		$currentOccu = $_SESSION['cc']->getAtomByName($_SESSION['cc']->getEgoSoftGears(),$_SESSION['currentSoftName'])->occurence;
+		$currentOccu = getAtomByName($_SESSION['cc']->getEgoSoftGears(),$_SESSION['currentSoftName'])->occurence;
 		if(!$_SESSION['cc']->setOccurenceGear($_SESSION['currentSoftName'],$currentOccu+1)){
 			treatCreatorErrors($return, $_SESSION['cc']->getLastError());
 		}
 	}
 	
 	if($_POST['addOccurence'] == "MORPH"){
-		$currentOccu = $_SESSION['cc']->getAtomByName($_SESSION['cc']->getGearForMorphName($_SESSION['currentMorph']),$_SESSION['currentMorphGearName'])->occurence;
+		$currentOccu = getAtomByName($_SESSION['cc']->getGearForMorphName($_SESSION['currentMorph']),$_SESSION['currentMorphGearName'])->occurence;
 		if(!$_SESSION['cc']->setOccurenceGear($_SESSION['currentMorphGearName'],$currentOccu+1,$_SESSION['currentMorph'])){
 			treatCreatorErrors($return, $_SESSION['cc']->getLastError());
 		}
@@ -1098,14 +1099,14 @@ if(isset($_POST['remOccurence'])){
 	}
 	
 	if($_POST['remOccurence'] == "SOFT"){
-		$currentOccu = $_SESSION['cc']->getAtomByName($_SESSION['cc']->getEgoSoftGears(),$_SESSION['currentSoftName'])->occurence;
+		$currentOccu = getAtomByName($_SESSION['cc']->getEgoSoftGears(),$_SESSION['currentSoftName'])->occurence;
 		if(!$_SESSION['cc']->setOccurenceGear($_SESSION['currentSoftName'],$currentOccu-1)){
 			treatCreatorErrors($return, $_SESSION['cc']->getLastError());
 		}
 	}
 	
 	if($_POST['remOccurence'] == "MORPH"){
-		$currentOccu = $_SESSION['cc']->getAtomByName($_SESSION['cc']->getGearForMorphName($_SESSION['currentMorph']),$_SESSION['currentMorphGearName'])->occurence;
+		$currentOccu = getAtomByName($_SESSION['cc']->getGearForMorphName($_SESSION['currentMorph']),$_SESSION['currentMorphGearName'])->occurence;
 		if(!$_SESSION['cc']->setOccurenceGear($_SESSION['currentMorphGearName'],$currentOccu-1,$_SESSION['currentMorph'])){
 			treatCreatorErrors($return, $_SESSION['cc']->getLastError());
 		}

src/Creator/version4/tertiary-choice/morphBMD.php
@@ -1,10 +1,11 @@
 <?php
 require_once '../../../php/EPCharacterCreator.php'; //BMD stand for : Bonus Malus Description
+require_once '../../../php/EPAtom.php';
 include('../other/bonusMalusLayer.php');
 include('../other/bookPageLayer.php');
 session_start();
 $currentMorphsList = $_SESSION['cc']->getCurrentMorphs();
-$currentMorph = $_SESSION['cc']->getAtomByName($currentMorphsList,$_SESSION['currentMorph']);
+$currentMorph = getAtomByName($currentMorphsList,$_SESSION['currentMorph']);
 if($currentMorph == null){
     $currentMorph = $_SESSION['cc']->getMorphByName($_SESSION['currentMorph']);
 }

src/Creator/version4/tertiary-choice/morphNegTraits.php
@@ -18,11 +18,11 @@ $currentMorph = $_SESSION['cc']->getCurrentMorphsByName($_SESSION['currentMorph'
                $m->cpCost > 0){
             	echo "<li>";
                     
-            	if($defaultTrait != null && $_SESSION['cc']->isAtomInArrayByName($m->name,$defaultTrait)){
+            	if($defaultTrait != null && $m->isInArray($defaultTrait)){
 	            	echo "		<label class='morphNegTrait selNegTrait' id='".$m->name."'>".$m->name.getListStampHtml($m->name)."</label><label class='costInfo'>(Granted)</label><span class='selectedicone selNegTrait selMorphNegTraitIcon' id='".$m->name."' data-icon='&#x2b;'></span>";
 
             	}
-            	else if($currentTraits != null && $_SESSION['cc']->isAtomInArrayByName($m->name,$currentTraits)){
+            	else if($currentTraits != null && $m->isInArray($currentTraits)){
             		echo "		<label class='morphNegTrait selNegTrait' id='".$m->name."'>".$m->name.getListStampHtml($m->name)."</label><label class='costInfo'>(".$m->cpCost." cp)</label><span class='selectedicone selNegTrait selMorphNegTraitIcon' id='".$m->name."' data-icon='&#x2b;'></span>";
             	}
             	else{

src/Creator/version4/tertiary-choice/morphNeuTraits.php
@@ -17,11 +17,11 @@ $currentMorph = $_SESSION['cc']->getCurrentMorphsByName($_SESSION['currentMorph'
                $m->cpCost == 0){
             	echo "<li>";
                     
-            	if($defaultTrait != null && $_SESSION['cc']->isAtomInArrayByName($m->name,$defaultTrait)){
+            	if($defaultTrait != null && $m->isInArray($defaultTrait)){
 	            	echo "		<label class='morphNegTrait selNeuTrait' id='".$m->name."'>".$m->name.getListStampHtml($m->name)."</label><label class='costInfo'>(Granted)</label><span class='selectedicone selNeuTrait selMorphNeuTraitIcon' id='".$m->name."' data-icon='&#x2b;'></span>";
 
             	}
-            	else if($currentTraits != null && $_SESSION['cc']->isAtomInArrayByName($m->name,$currentTraits)){
+            	else if($currentTraits != null && $m->isInArray($currentTraits)){
             		echo "		<label class='morphNeuTrait selNeuTrait' id='".$m->name."'>".$m->name.getListStampHtml($m->name)."</label><span class='selectedicone selNeuTrait selMorphNeuTraitIcon' id='".$m->name."' data-icon='&#x2b;'></span>";
             	}
             	else{

src/Creator/version4/tertiary-choice/morphPosTraits.php
@@ -16,11 +16,11 @@ $currentMorph = $_SESSION['cc']->getCurrentMorphsByName($_SESSION['currentMorph'
                isTraitLegal($currentMorph,$m) &&
                $m->cpCost > 0){
             	echo "<li>";
-            	if($defaultTrait != null && $_SESSION['cc']->isAtomInArrayByName($m->name,$defaultTrait)){
+            	if($defaultTrait != null && $m->isInArray($defaultTrait)){
 	            	echo "		<label class='morphPosTrait selPosTrait' id='".$m->name."'>".$m->name.getListStampHtml($m->name)."</label><label class='costInfo'>(Granted)</label><span class='selectedicone selPosTrait selMorphPosTraitIcon' id='".$m->name."' data-icon='&#x2b;'></span>";
 
             	}
-            	else if($currentTraits != null && $_SESSION['cc']->isAtomInArrayByName($m->name,$currentTraits)){
+            	else if($currentTraits != null && $m->isInArray($currentTraits)){
             		echo "		<label class='morphPosTrait selPosTrait' id='".$m->name."'>".$m->name.getListStampHtml($m->name)."</label><label class='costInfo'>(".$m->cpCost." cp)</label><span class='selectedicone selPosTrait selMorphPosTraitIcon' id='".$m->name."' data-icon='&#x2b;'></span>";
             	}
             	else{

commit f7e5d9af5330c4c124e4caf5a3d1772f2a5737ef

Allow morph traits to be properly removed again
Regression caused by: 20d2b70b7979fda0ca5480ce21233a3dec7ef11f

EPAtom and subclasses EPSKill, EPTrait now use a `match` function.
Previously, removing a subclassed atom would fail if they were identical, but had different uids.

src/php/EPAtom.php
@@ -124,11 +124,19 @@ class EPAtom {
         return preg_replace($replace_char, '_', $input);
     }
 
+    //Check if two atoms are the same
+    function match($atom){
+        if (strcmp($atom->getUid(),$this->atomUid) == 0){
+            return true;
+        }
+        return false;
+    }
+
     // Check if this Atom is in the array
     public function isInArray($array){
         if (!empty($array)){
             foreach ($array as $item){
-                if (strcmp($item->getUid(),$this->atomUid) == 0){
+                if ($this->match($item)){
                     return true;
                 }
             }
@@ -150,11 +158,12 @@ class EPAtom {
     // Remove this Atom from an array, returns false on failure
     public function removeFromArray(&$array){
         if (!$this->isInArray($array)){
+            error_log("Not in array!");
             return false;
         }else{
             $index = 0;
-            foreach ($array as $value) {
-                if (strcmp($value->getUid(),$this->getUid()) == 0){
+            foreach ($array as $item) {
+                if ($this->match($item)){
                     break;
                 }else{
                     $index++;

src/php/EPSkill.php
@@ -172,13 +172,9 @@ class EPSkill extends EPAtom{
 
     // Check for duplicate skills by name AND prefix.
     // This is safe, and prevents users from adding duplicate skills
-    public function isInArray($list){
-        if (is_array($list)){
-            foreach ($list as $l){
-                if (strcasecmp($l->name,$this->name) == 0 && strcasecmp($l->prefix,$this->prefix) == 0){
-                    return true;
-                }
-            }
+    public function match($skill){
+        if (strcasecmp($l->name,$this->name) == 0 && strcasecmp($l->prefix,$this->prefix) == 0){
+            return true;
         }
         return false;
     }

src/php/EPTrait.php
@@ -82,6 +82,21 @@ class EPTrait extends EPAtom{
         $this->canUse = $canUse;
     }
     
+    // Check for duplicate traits by checking all values.
+    // This is more expensive than EPAtom's version, but catches duplicate traits with different Uids
+    //Check if two atoms are the same
+    public function match($trait){
+        if (strcasecmp($trait->name,$this->name) == 0 &&
+            $trait->traitPosNeg===$this->traitPosNeg &&
+            $trait->traitEgoMorph===$this->traitEgoMorph &&
+            $trait->cpCost===$this->cpCost &&
+            $trait->level===$this->level &&
+            $trait->canUse===$this->canUse){
+                return true;
+        }
+        return false;
+    }
+
 }
 
 ?>