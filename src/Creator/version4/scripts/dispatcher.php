Eclipse-Phase-Unofficial/ep-character-creator

commit ec10f4c551038881a17e5d2f17c4a802c20260d9

Reworked loading code
User is no longer trapped if they attempt to load an invalid file.
User can close the loading popup without triggering a page reload.
File load errors now work the same way as every other non critical error.
Loading bar displays as page reloads.  (Error checking is near instant)
File upload occurs immediately after selection.  (When the user clicks load, the file is already on the server)
Default is now to load into creation mode.

Note:
In order to support older browsers I couldn't just use pure ajax.
File selection and uploading now happens by a tiny embedded iframe.

src/Creator/version4/scripts/dispatcher.php
@@ -54,8 +54,6 @@ function treatCreatorErrors(&$data,$creatorError){
 //INIT
 $return = array();
 $return['error'] = false;
-$_SESSION['versioningFault'] = false;
-$return['versioningFault'] = false; 
 $provider = new EPListProvider('../../../php/config.ini');
 
 	//error_log(print_r($_POST,true));
@@ -63,47 +61,41 @@ $provider = new EPListProvider('../../../php/config.ini');
 	//error_log(print_r($_SESSION,true));
 
 //if a file to load LOAD FILE
-if (isset($_SESSION['fileToLoad'])) {
-    $_SESSION['cc'] = new EPCharacterCreator("../../../php/config.ini");
-    $_SESSION['cc']->back = new EPCharacterCreator("../../../php/config.ini");		
+if (isset($_POST['load_char'])) {
+    if(isset($_SESSION['fileToLoad'])){
+        $saveFile = json_decode($_SESSION['fileToLoad'],true);
 
-    $saveFile = json_decode($_SESSION['fileToLoad'],true);
- 
-    if (!empty($saveFile['versionNumber']) && floatval($saveFile['versionNumber']) >= $_SESSION['cc']->configValues->getValue('GeneralValues','versionNumberMin')){	
-		$_SESSION['cc']->loadSavePack($saveFile);
-        $_SESSION['cc']->back->loadSavePack($saveFile);
-        $_SESSION['cc']->back->setMaxRepValue($_SESSION['cc']->configValues->getValue('RulesValues','EvoMaxRepValue'));
-        $_SESSION['cc']->setMaxRepValue($_SESSION['cc']->configValues->getValue('RulesValues','EvoMaxRepValue'));
-        $_SESSION['cc']->back->setMaxSkillValue($_SESSION['cc']->configValues->getValue('RulesValues','SkillEvolutionMaxPoint'));
-        $_SESSION['cc']->setMaxSkillValue($_SESSION['cc']->configValues->getValue('RulesValues','SkillEvolutionMaxPoint'));
-                    
-        if ($_SESSION['cc']->creationMode == false){ //We are alreay in evo mode
-            $_SESSION['creationMode'] = false; //We force evo mode
-            $_SESSION['cc']->evoRezPoint += $_SESSION['rezPoints'];
-			$_SESSION['cc']->evoRepPoint += $_SESSION['repPoints'];
-			$_SESSION['cc']->evoCrePoint += $_SESSION['credPoints'];
-        }else{//We are in creation mode
-            if(!$_SESSION['creationMode']){//We pass in evo mode
-                $_SESSION['cc']->creationMode = false;
-                $_SESSION['cc']->evoCrePoint = $_SESSION['cc']->getCredit(); // we keep credits from creation mode
-				$_SESSION['cc']->evoRezPoint += $_SESSION['rezPoints'];
-				$_SESSION['cc']->evoRepPoint += $_SESSION['repPoints'];
-				$_SESSION['cc']->evoCrePoint += $_SESSION['credPoints'];
-            }else{//We stay in creation mode
-                $_SESSION['cc']->creationMode = true;
-            }            
+        if (!empty($saveFile['versionNumber']) && floatval($saveFile['versionNumber']) >= $_SESSION['cc']->configValues->getValue('GeneralValues','versionNumberMin')){
+            $_SESSION['cc'] = new EPCharacterCreator("../../../php/config.ini");
+            $_SESSION['cc']->back = new EPCharacterCreator("../../../php/config.ini");
+
+            $_SESSION['cc']->loadSavePack($saveFile);
+            $_SESSION['cc']->back->loadSavePack($saveFile);
+            $_SESSION['cc']->back->setMaxRepValue($_SESSION['cc']->configValues->getValue('RulesValues','EvoMaxRepValue'));
+            $_SESSION['cc']->setMaxRepValue($_SESSION['cc']->configValues->getValue('RulesValues','EvoMaxRepValue'));
+            $_SESSION['cc']->back->setMaxSkillValue($_SESSION['cc']->configValues->getValue('RulesValues','SkillEvolutionMaxPoint'));
+            $_SESSION['cc']->setMaxSkillValue($_SESSION['cc']->configValues->getValue('RulesValues','SkillEvolutionMaxPoint'));
+
+            // Save pack and user both say we are in creation mode
+            if ($_SESSION['cc']->creationMode == true && $_POST['creationMode'] == true ){
+                $_SESSION['cc']->creationMode = true; //We stay in creation mode
+            }else{
+                // We force evo mode
+                $_SESSION['creationMode'] = false;
+                $_SESSION['cc']->evoRezPoint += $_POST['rezPoints'];
+                $_SESSION['cc']->evoRepPoint += $_POST['repPoints'];
+                $_SESSION['cc']->evoCrePoint += $_POST['credPoints'];
+            }
+
+            if (!empty($_SESSION['cc']->character->morphs)){
+                $_SESSION['cc']->activateMorph($_SESSION['cc']->character->morphs[0]);
+            }
+            $_SESSION['cc']->adjustAll();
+        }else{
+            treatCreatorErrors($return,new EPCreatorErrors("Incompatible file version!",EPCreatorErrors::$SYSTEM_ERROR));
         }
-        
-        if (!empty($_SESSION['cc']->character->morphs)){
-            $_SESSION['cc']->activateMorph($_SESSION['cc']->character->morphs[0]);
-        }
-                
-		$_SESSION['fileToLoad'] = null; 
-        $_SESSION['cc']->adjustAll();            
-	}else{ 
-        $_SESSION['versioningFault'] = true;
-        $_SESSION['fileToLoad'] = null;  
-        $return['versioningFault'] = true; 
+    }else{
+        treatCreatorErrors($return,new EPCreatorErrors("No File Selected!",EPCreatorErrors::$SYSTEM_ERROR));
     }
 }
 

src/Creator/version4/css/popup.css
@@ -148,7 +148,7 @@
     text-align: justify;
     top: 42%;
     display: none;
-    z-index: 10;
+    z-index: 11;
 
     width: 25%;
     height: 3%;

src/Creator/version4/index.php
@@ -1,15 +1,5 @@
 <?php 
 	error_reporting(0);
-	session_start();
-	if(isset($_FILES['uploadedfile'])){
-		$handle = fopen($_FILES['uploadedfile']['tmp_name'],'r');
-		$_SESSION['fileToLoad'] = fread($handle, filesize($_FILES['uploadedfile']['tmp_name']));
-		fclose($handle);
-		$_SESSION['creationMode'] = isset($_POST['creationMode']);
-		$_SESSION['rezPoints'] = $_POST['rezPoints'];
-	    $_SESSION['repPoints'] = $_POST['repPoints'];
-	    $_SESSION['credPoints'] = $_POST['credPoints'];
-	}
 ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->

src/Creator/version4/other/upload_file.php
@@ -1,0 +1,42 @@
+<?php
+    error_reporting(0);
+    session_start();
+
+//     error_log(print_r($_POST,true));
+//     error_log(print_r($_FILES,true));
+//     error_log(print_r($_SESSION,true));
+
+    if(isset($_POST['clear_file'])){
+        unset($_SESSION['fileName']);
+        unset($_SESSION['fileToLoad']);
+    }
+    else if(isset($_FILES['uploadedfile'])){
+        $_SESSION['fileName']=$_FILES['uploadedfile']['name'];
+        $handle = fopen($_FILES['uploadedfile']['tmp_name'],'r');
+        $_SESSION['fileToLoad'] = fread($handle, filesize($_FILES['uploadedfile']['tmp_name']));
+    }
+?>
+<html>
+<head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+</head>
+<body style="margin:0;">
+    <form id="file_upload_form" method="POST" enctype="multipart/form-data">
+        <input type="hidden" name="MAX_FILE_SIZE" value="8388608" /><!--Max size of 8MB.  If we're hitting this limit there's a problem.-->
+        <?php
+            // Offer to set the file name, or clear the uploaded file
+            if(isset($_SESSION['fileName'])){
+                echo "<input type='hidden' name='clear_file' value='true' />";
+                echo "<button id='fileName' name='fileName' value='".$_SESSION['fileName']."'>".$_SESSION['fileName']."</button>";
+            }
+            else {
+                echo "<input id='uploadedfile' name='uploadedfile' type='file'>";
+            }
+        ?>
+    </form>
+    <script>
+        $('#uploadedfile').on('change', function(){ $('#file_upload_form').submit(); });
+        $('#fileName').on('click', function(){ $('#file_upload_form').submit(); });
+    </script>
+</body>
+

src/Creator/version4/popup-contents/load.php
@@ -1,7 +1,4 @@
-<?php
-session_start();
-?>
-<form action="index.php" id="loadForm" method="POST" enctype="multipart/form-data">
+<form id="loadForm" method="POST">
 	<table class="popup_table" id="table_load" align="center">
 		<tr align="center">
 			<td>
@@ -13,16 +10,14 @@ session_start();
 			<td>
 				If you have save a character you can reload it on the system for evolving or changing details.
 				<br><br>
-				
-				<input type="hidden" name="MAX_FILE_SIZE" value="1000000000" />
-				Choose a file to upload: <input id="fileName" name="uploadedfile" type="file">
+				Choose a file: <iframe style="position: absolute; border:none;" src="other/upload_file.php"></iframe>
 				<br><br>
 			</td>
 		</tr>
 		
 		 <tr align="center">	
 			<td>
-				<input type="checkbox" name="creationMode" value="cre"/> Check if you want to continue on creation mode.
+				<input type="checkbox" id="creationMode" name="creationMode" value="cre" checked/> Check if you want to continue on creation mode.
 				<br><br>
 				I have earned <input style="line-height: 1em;" id="rezPoints" name="rezPoints" type="number" min="0" value="0"> Rez points.
 				<br><br>
@@ -32,30 +27,17 @@ session_start();
 				<br><br>
 			</td>
 		</tr>
-		
-		<tr align="center">
-			<td>
-				<div id="errorLoadMsg">
-					<?php
-						if($_SESSION['versioningFault']){
-							echo "<label style='color:red'>Your file version is too old and no more compatible, sorry.</label>";
-						}
-						$_SESSION['versioningFault'] = false;
-					?>
-				</div>
-			</td>
-		</tr>
-		
+
 		<tr align="center">
 			<td>
 				<br><br>
-				<button class="loadLoadButton popupInnerButton">
+				<button type="submit" class="popupInnerButton">
 			       			Load 
 		        </button>
-		        </form>
-		        <button class="closeButton popupInnerButton">
+		        <button type="button" class="closeButton popupInnerButton">
 			       			Cancel
 		        </button>
 			</td>	
 		</tr>
 	</table>
+</form>

src/Creator/version4/scripts/ajaxManager.js
@@ -29,20 +29,14 @@ $(document).ready(function(){
                         getCrePoint : 'get'
                 },
                 function(response){
-                		if(response.versioningFault){
-	                        closeAllPopup();
-	                        endLoading();
-                            loadPopup("#load_popup","popup-contents/load.php");
-                        }
-                        else if(response.sessionExist){
-	                        setRemainingPoint(response);
-	                        loaddingLoad();
-                        }
-                        else{
-	                        closeAllPopup();
-	                        endLoading();
-				        	loadPopup("#reset_popup","popup-contents/reset.php");
-                        }
+                    if(response.sessionExist){
+                        setRemainingPoint(response);
+                        loaddingLoad();
+                    }
+                    else{
+                        endLoading();
+                        loadPopup("#reset_popup","popup-contents/reset.php");
+                    }
                 });
             firstTime = false;
         }

src/Creator/version4/scripts/popup.js
@@ -5,19 +5,15 @@
 
 function loaddingReset(){
 	setTimeout(function(){
+		closeAllPopup();
 		location.reload();
-		$("#reset_popup").css('opacity',0);
-		$("#reset_popup").css('visibility','hidden');
-	    closeAllPopup();
 	},1000);
 }
 
 function loaddingLoad(){
 	setTimeout(function(){
+		closeAllPopup();
 		endLoading();
-		$("#reset_popup").css('opacity',0);
-		$("#reset_popup").css('visibility','hidden');
-	    closeAllPopup();
 	},3000);
 }
 
@@ -34,8 +30,7 @@ function endLoading(){
 //**********Popup Helpers**********
 
 function closeAllPopup(){
-    if($("#load_popup").css('visibility') == 'visible' ||
-       $("#reset_popup").css('visibility') == 'visible'||
+    if($("#reset_popup").css('visibility') == 'visible'||
        $("#error_popup").css('visibility') == 'visible'){
         location.reload();
     }
@@ -146,11 +141,6 @@ $(document).on("click",".startButton",function() {
     return false;
 });
 
-//Click on the load load button
-$(document).on("click",".loadLoadButton",function() {
-    $('#loadForm').submit();
-});
-
 //Click on the save save button
 $(document).on("click",".saveSaveButton",function() {
     $('#saveForm').submit();
@@ -159,13 +149,22 @@ $(document).on("click",".saveSaveButton",function() {
 
 //Load file
 $(document).on('submit','#loadForm', function () {
-    var fileVal = $('#fileName').val();
-    if(fileVal == ""){
-        $('#errorLoadMsg').html("Choose a file !");
-        return false;
-    }
-    else{
-        closeAllPopup();
-    }
+    startLoading();
+    ajax_helper({
+            load_char :     true,
+            creationMode:   $('#creationMode').prop("checked"),
+            rezPoints:      $('#rezPoints').val(),
+            repPoints:      $('#repPoints').val(),
+            credPoints:     $('#credPoints').val()
+        },
+        function(response){
+            if(response.error) {
+                endLoading();
+                treatMessageError(response);
+            }else{
+                location.reload();
+                return false;
+            }
+        });
+    return false;
 });
-

commit 5b2eed4a3fb23682b0428858a4e2280da159e35b

Fixed an error that always forced the user into creation mode
Regression caused by:  ec10f4c5

src/Creator/version4/scripts/dispatcher.php
@@ -79,11 +79,11 @@ if (isset($_POST['load_char'])) {
             $_SESSION['cc']->setMaxSkillValue($_SESSION['cc']->configValues->getValue('RulesValues','SkillEvolutionMaxPoint'));
 
             // Save pack and user both say we are in creation mode
-            if ($_SESSION['cc']->creationMode == true && $_POST['creationMode'] == true ){
+            if ($_SESSION['cc']->creationMode == true && $_POST['creationMode'] == "true" ){
                 $_SESSION['cc']->creationMode = true; //We stay in creation mode
             }else{
                 // We force evo mode
-                $_SESSION['creationMode'] = false;
+                $_SESSION['cc']->creationMode = false;
                 $_SESSION['cc']->evoRezPoint += $_POST['rezPoints'];
                 $_SESSION['cc']->evoRepPoint += $_POST['repPoints'];
                 $_SESSION['cc']->evoCrePoint += $_POST['credPoints'];