a079d33383f35861dc1a2342540b68d8d4f7f68c
Arthur Moore <Arthur.Moore.git@cd-net.net>
2017-01-22 20:37:29 -0500

    Add a small script to generate documentation using apigen
    Also does a quick check for any glaring errors in the PHP files.

---
+++ Documentation/build_documentation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Build PHP Documentation
+apigen generate -s src -d Documentation/api
+
+# Check for syntax errrors in all php documents
+for i in `find ./src -iname '*.php'`;
+do php -l $i;
+done;
+
+#Check php assumptions
+#composer require --dev rskuipers/php-assumptions
+#apt-get install php-mbstring
+# ./vendor/bin/phpa ../src/ > problems.txt
