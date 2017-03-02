b476db5e0595c682c110a67ee84a3aae6b5afedc
Arthur Moore <Arthur.Moore.git@cd-net.net>
2017-03-02 10:09:06 -0500

    gearHelper.php:  Documentation update

--- src/Creator/version4/other/gearHelper.php
+++ src/Creator/version4/other/gearHelper.php
@@ -1,6 +1,14 @@
 <?php
 require_once('bookPageLayer.php');
 
+
+/**
+ * Tell the user how much something costs.
+ *
+ * @param $cost      - The item's cost. (if 0, nothing is displayed; takes precidence over $isDefault)
+ * @param $isDefault - If the item is automatically given to the player. (if true, "(Granted)" is displayed)
+ * @returns The HTML for a 'costInfo' <span\>.
+ */
 function getCostHtml($cost,$isDefault){
     $costDisplay = "(".$cost." cp)";
     if($isDefault){
@@ -12,14 +20,17 @@ function getCostHtml($cost,$isDefault){
     return "<span class='costInfo'>".$costDisplay."</span>";
 }
 
-// Generate a generic li element used for selecting/deselecting an item
-//
-// $item:       The item being selected or deselected
-// $itemClass:  Used to determine what javascript function is activated when the item is clicked
-// $cpCost:     How much the item costs
-// $granted:    If the item is a default (non-removable)
-// $checked:    If the item is checked, or has a plus icon
-// $iconClass:  used to determine what javascript function is activated when the check or plus icon is selected
+/**
+ * Generate a generic li element used for selecting/deselecting an item
+ *
+ * @param $item       - The item being selected or deselected
+ * @param $itemClass  - Used to determine what javascript function is activated when the item is clicked
+ * @param $cpCost     - How much the item costs
+ * @param $granted    - If the item is a default (non-removable)
+ * @param $checked    - If the item is checked, or has a plus icon
+ * @param $iconClass  - Used to determine what javascript function is activated when the check or plus icon is selected
+ * @returns The HTML for a <li\> element.
+ */
 function getFormattedLi($item,$itemClass,$cpCost,$granted,$checked,$iconClass){
     $htmlResult  = "";
     $htmlResult .= "<li class='".$itemClass."' id='".$item->name."'>";
