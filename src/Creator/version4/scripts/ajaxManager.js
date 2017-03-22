Eclipse-Phase-Unofficial/ep-character-creator

commit 5261dbbff785fd8f6cb9144074835b940308e76c

Add more error checking when the user attempts to add a skill

src/Creator/version4/scripts/ajaxManager.js
@@ -475,6 +475,10 @@ $(document).ready(function(){
 		
 		//add a temp active skill
 		$(document).on('click', '#addActSkill' ,function () {
+            if(!$('#actToAdd').val()){
+                displayRulesMessage("Attempting to add a blank skill!");
+                return false;
+            }
                 do_ajax( {
                             newTmpActSkill : $('#actToAdd').val(),
                             newTmpSkillPrefix : $('#actprefix').val()
@@ -545,6 +549,10 @@ $(document).ready(function(){
 		});
 		//Add the native language
 		$(document).on('click', '#addNativeLanguage' ,function () {
+            if(!$('#langToAdd').val()){
+                displayRulesMessage("Attempting to add a blank skill!");
+                return false;
+            }
                 do_ajax( {
                             newNatLanguageSkill : $('#langToAdd').val()
                     },
@@ -569,6 +577,10 @@ $(document).ready(function(){
 		
 		//add a temp knowledge  skill
 		$(document).on('click', '#addKnowSkill' ,function () {
+            if(!$('#addKnowSkill').val()){
+                displayRulesMessage("Attempting to add a blank skill!");
+                return false;
+            }
                 do_ajax( {
                             newTmpKnoSkill : $('#knoToAdd').val(),
                             newTmpSkillPrefix : $('#knoprefix').val()

src/Creator/version4/scripts/dispatcher.php
@@ -393,7 +393,7 @@ if(isset($_POST['skill'])){
 }
 
 //ADD TMP ACTIVE SKILL
-if(isset($_POST['newTmpActSkill'])){
+if( isset($_POST['newTmpActSkill']) && !empty($_POST['newTmpActSkill']) ){
 	//error_log("adding active skill \"" . $_POST['newTmpSkillPrefix'] . "\": \"" . $_POST['newTmpActSkill']."\"");
 	if(!$_SESSION['cc']->addSkill($_POST['newTmpActSkill'], 
 									  $provider->getAptForPrefix($_POST['newTmpSkillPrefix']),
@@ -405,7 +405,7 @@ if(isset($_POST['newTmpActSkill'])){
 }
 
 //ADD TMP KNOWLEDGE SKILL
-if(isset($_POST['newTmpKnoSkill'])){
+if( isset($_POST['newTmpKnoSkill']) && !empty($_POST['newTmpKnoSkill']) ){
 	//error_log("adding knowledge skill  \"" . $_POST['newTmpSkillPrefix'] . "\": \"" . $_POST['newTmpKnoSkill']."\"");
 	if(!$_SESSION['cc']->addSkill($_POST['newTmpKnoSkill']." ", 
 									  $provider->getAptForPrefix($_POST['newTmpSkillPrefix']),
@@ -417,7 +417,7 @@ if(isset($_POST['newTmpKnoSkill'])){
 }
 
 //ADD NATIVE LANGUAGE SKILL
-if(isset($_POST['newNatLanguageSkill'])){
+if( isset($_POST['newNatLanguageSkill']) && !empty($_POST['newNatLanguageSkill']) ){
 	if(!$_SESSION['cc']->addSkill($_POST['newNatLanguageSkill'], 
 									  $provider->getAptForPrefix('Language'),
 									  EPSkill::$KNOWLEDGE_SKILL_TYPE, 

commit 56f4d1ec070009ded45832d892793c3f39af3c50

Fix being unable to add temporary Knowledge Skills
Regression caused by commit: 5261dbbff785fd8f6cb9144074835b940308e76c

src/Creator/version4/scripts/ajaxManager.js
@@ -406,7 +406,7 @@ $(document).ready(function(){
 		
 		//add a temp knowledge  skill
         function addKnowSkill() {
-            if(!$('#addKnowSkill').val()){
+            if(!$('#knoToAdd').val()){
                 displayRulesMessage("Attempting to add a blank skill!");
                 return false;
             }