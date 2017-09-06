a0767a72239575bcce8d59d8aa7c2366f1a478d1
Russell Bewley <rbewley4@gmail.com>
2017-09-05 19:46:18 -0700

    Remove devbox code.
    The development VM, devbox, that was provisioned by Vagrant and Puppet,
    has been replaced by Docker.

--- devbox/puppet/modules/lamp/manifests/params.pp
+++
@@ -1,6 +0,0 @@
-class lamp::params {
-  $lamp_package_name = "lamp-server^"
-  $lamp_package = Package[$lamp_package_name]
-  $apache_service_name = "apache2"
-  $apache_service = Service[$apache_service_name]
-}
