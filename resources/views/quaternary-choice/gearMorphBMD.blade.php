55bdb57046ffb7f07b457d2b9b328b3978ee2b55
Arthur Moore <Arthur.Moore.git@cd-net.net>
2018-11-03 23:32:37 -0400

    gearMorphBMD: Fix a missing close parens

--- resources/views/quaternary-choice/gearMorphBMD.blade.php
+++ resources/views/quaternary-choice/gearMorphBMD.blade.php
@@ -7,7 +7,7 @@ use App\Creator\DisplayHelpers\Helpers;
 
 // $hint = "--dubug:morph";
 $morphGears = EpDatabase()->getGearForMorphName((string) session('currentMorph'));
-$currentGear = EPAtom::getAtomByName($morphGears, (string) session('currentMorphGearName');
+$currentGear = EPAtom::getAtomByName($morphGears, (string) session('currentMorphGearName'));
 if($currentGear == null){
     $currentGear =  EpDatabase()->getGearByName((string) session('currentMorphGearName'));
     // $hint = "--debug:general";
